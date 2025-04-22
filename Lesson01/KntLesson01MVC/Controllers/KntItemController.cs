867f2965e883cd1afb19bdc27421104c5a1f7195
KimTam05 <kimngoctam15@gmail.com>
2025-04-22 21:09:15 +0700

    Lesson 01

---
+++ Lesson01/KntLesson01MVC/Controllers/KntItemController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KntLesson01MVC.Controllers
+{
+    public class KntItemController : Controller
+    {
+        public IActionResult KntItem()
+        {
+            return View();
+        }
+    }
+}
