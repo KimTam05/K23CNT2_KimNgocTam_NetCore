86e3f3c7bae06836eeeea565a7e976ac9c7b3784
KimTam05 <kimngoctam15@gmail.com>
2025-04-28 09:08:41 +0700

    Lesson 02

---
+++ Lesson02/KntLesson02/KntLesson02/Controllers/KntHomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using KntLesson02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KntLesson02.Controllers
+{
+    public class KntHomeController : Controller
+    {
+        private readonly ILogger<KntHomeController> _logger;
+
+        public KntHomeController(ILogger<KntHomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lesson02/KntLesson02/KntLesson02/Controllers/KntProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KntLesson02.Controllers
+{
+    public class KntProductController : Controller
+    {
+        public IActionResult KntProduct()
+        {
+            ViewData["messageData"] = "Data From ViewData";
+            ViewBag.messageBag = "Data From ViewBag";
+            TempData["messageTempData"] = "Data From TempData";
+            return View();
+        }
+    }
+}
